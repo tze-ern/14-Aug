3550c13f7a3dc9ccf0059278ee112c89906afea0
tze-ern <65218242+tze-ern@users.noreply.github.com>
2021-08-18 20:26:44 +0800

    Update LCD_clicker.ino

--- LCD_clicker.ino
+++ LCD_clicker.ino
@@ -3,26 +3,24 @@ int counter = 0;
 int x;
 char *words[]={"Rampage", "Legendary", "Godlike", "Savage","unstoppable"};
 long words1;
+bool shownText = 0;
 
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x3F for a 16 chars and 2 line display
-
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 void setup() {
   pinMode(buttonPin, INPUT_PULLUP);
   
   Serial.begin(9600);
-  randomSeed(666);
   
   lcd.init();
   lcd.clear();
-  lcd.backlight();      // Make sure backlight is on
-
-  // Print a message on both lines of the LCD.
-  lcd.setCursor(2, 0);  //Set cursor to character 2 on line 0
+  lcd.backlight();
+  
+  lcd.setCursor(2, 0);
   lcd.print("Clicks:");
 
-  lcd.setCursor(2, 1);  //Move cursor to character 2 on line 1
+  lcd.setCursor(2, 1);
   lcd.print(counter);
 }
 
@@ -30,60 +28,50 @@ void loop() {
   int buttonState;
   buttonState = digitalRead(buttonPin);
 
-  if (buttonState == LOW) // light the LED
+  if (buttonState == LOW) 
   {
     counter++;
     lcd.clear();
-    lcd.setCursor(2, 0);  //Set cursor to character 2 on line 0
+    lcd.setCursor(2, 0); 
     lcd.print("Clicks:");
-    lcd.setCursor(2, 1);  //Move cursor to character 2 on line 1
+    lcd.setCursor(2, 1);
     lcd.print(counter);
 
     delay(150);
   }
-
+shownText = 0;
     if (counter == 10)
   {
-    lcd.clear();
-    lcd.setCursor(2, 0);  //Set cursor to character 2 on line 0
-    Random();
-    delay(500);
+  Random();
+  }
+    if (counter == 20)
+  {
+  Random();
+  }
+    if (counter == 30)
+  {
+  Random();
+  }
+    if (counter == 40)
+  {
+  Random();
+  }
+    if (counter == 50)
+  {
+  Random();
   }
 
-  else if (counter == 20)
-  {
-    lcd.clear();
-    lcd.setCursor(2, 0);  //Set cursor to character 2 on line 0
-    Random();
-    delay(500);
-  }
-
-  else if (counter == 30)
-  {
-    lcd.clear();
-    lcd.setCursor(2, 0);  //Set cursor to character 2 on line 0
-    Random();
-    delay(500);
-  }
-
-  else if (counter == 40)
-  {
-    lcd.clear();
-    lcd.setCursor(2, 0);  //Set cursor to character 2 on line 0
-    Random();
-    delay(500);
-  }
-
-  else if (counter == 50)
-  {
-    lcd.clear();
-    lcd.setCursor(2, 0);  //Set cursor to character 2 on line 0
-    Random();
-    delay(500);
-  }
 }
 
 void Random() {
   words1 = random(sizeof(words)/sizeof(char*));
-  lcd.print(words[words1]);
+     if (shownText = 0)
+    {
+    lcd.clear();
+    lcd.setCursor(2, 0); 
+    lcd.print(words[words1]);
+    delay(500);
+    shownText = 1;
+    }
+
 }
